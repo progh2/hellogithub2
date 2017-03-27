aff2822233315cbd00519b02f2f25fdf4c29bdf1
Gihun Ham <ham@e-mirim.hs.kr>
2017-03-27 20:16:49 +0900

    Init Project

---
+++ app/src/main/java/kr/hs/emirim/progh2/hellogithub/MainActivity.java
@@ -0,0 +1,13 @@
+package kr.hs.emirim.progh2.hellogithub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
